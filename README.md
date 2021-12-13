9bbf6511cdf2cbe28e1ef066fdaad07af9dfa8c5
Iuliia-Shchur <79912506+Iuliia-Shchur@users.noreply.github.com>
2021-12-14 01:28:36 +0200

    links

---
+++ README.md
@@ -0,0 +1,11 @@
+//Link-1
+https://ibb.co/VQRk71S
+
+//Link-2
+https://ibb.co/CPt63dd
+
+//Link-3
+https://ibb.co/wMVCbvH
+
+//Link-4
+https://ibb.co/wJH9nBG
---
+++ index.js
@@ -0,0 +1,57 @@
+
+const chalk = require('chalk');
+const { Command } = require('commander');
+const {listContacts, addContact, getContactById, removeContact} = require('./contact');
+
+const program = new Command();
+program
+  .requiredOption('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
+
+program.parse(process.argv);
+
+const argv = program.opts();
+
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+    
+  switch (action) {
+    case 'list':
+      const contacts = await listContacts()
+      console.table(contacts)
+      break;
+
+    case 'get':
+      const logContactById = await getContactById(id)
+      if(logContactById) {
+          console.log(chalk.blue('Found contact'));
+          console.log(logContactById);
+          return
+
+      } 
+          console.log(chalk.cyan('Contact not found'));
+
+      
+      break;
+
+    case 'add':
+      const contact = await addContact(name, email, phone)
+      console.log(chalk.green('Added new contact'));
+      console.log(contact);
+      break;
+
+    case 'remove':
+     const updatedContacts = await removeContact(id)
+     console.log(chalk.greenBright(' Contact deleted successfully'));
+     console.table(updatedContacts);
+      break;
+
+    default:
+      console.warn(chalk.red('Unknown action type!'));
+  }
+}
+
+invokeAction(argv).then(() => console.log('Operation successful'));
